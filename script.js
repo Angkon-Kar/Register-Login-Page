ec4da74718fd46f771a1a7f0f32db772794aa16b
Angkon Kar <angkon310@gmail.com>
2025-07-11 20:35:00 +0600

    Added Function showLogin

---
+++ script.js
@@ -0,0 +1,7 @@
+function showLogin(){
+    document.getElementById('loginBtn').classList.add('active');
+    document.getElementById('registerBtn').classList.remove('active');
+    document.getElementById('loginForm').classList.add('active');
+    document.getElementById('registerForm').classList.remove('active');
+}
+
